1a4fdf655cd8d53e06d141e9f99c5290566d8c83
Dennis de Jesus <ddejesus@outlook.com>
2017-09-18 13:59:50 -0400

    Edited upload-portfolio-lambda.py

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,6 +2,8 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
+
 
 s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
@@ -14,5 +16,5 @@ build_bucket.download_fileobj('portfoliobuild.dmbackup.com/portfoliobuild.zip', 
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
